e907a23994ffd58cdfac127f2f4afdd16f4d7ed3
Sammymushagasha <Sammythompmush@gmail.com>
2025-07-20 21:27:46 -0400

    added button and eventListener to button to replace grid with new user dimensions

--- javascript.js
+++ javascript.js
@@ -11,11 +11,33 @@ for(let i = 0; i < 16 * 16; i++){
     grid_container.appendChild(block);
 }
 
-const resetButton = document.createElement("button");
-resetButton.classList.add("reset");
+const reset = document.querySelector(".reset");
 
-resetButton.textContent = "Reset";
-resetButton.style.color = "#e8e2f7ff";
-resetButton.style.backgroundColor = "#212121";
+reset.addEventListener("click", () => {
+    let new_grid = prompt("Enter new grid dimensions (No greater than 100");
+
+    new_grid = parseInt(new_grid);
+    if (isNaN(new_grid) || new_grid < 1 || new_grid > 100) {
+        alert("Please enter a valid number between 1 and 100.");
+        return;
+  }
+
+    grid_container.innerHTML = "";
+
+    for(let i = 0; i < new_grid * new_grid; i++){
+        const block = document.createElement("div");
+        block.classList.add('cell');
+        block.style.width = (600 / new_grid) + 'px';
+        block.style.height = (600 / new_grid) + 'px';
+
+
+        block.addEventListener("mouseover", () => {
+        block.style.backgroundColor = "#B2AC88"
+    })
+
+    grid_container.appendChild(block);
+}
+})
+
 
 
